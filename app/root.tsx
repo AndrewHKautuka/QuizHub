f33888a2d594d3b2f85df9b133f46330facd11b7
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-06-04 17:41:38 +0200

    Remove NavBar
    - Should be added on landing page and dashboard base

--- app/root.tsx
+++ app/root.tsx
@@ -47,7 +47,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
 	return (
 		<StyledEngineProvider injectFirst>
-			<NavBar />
 			<Outlet />
 		</StyledEngineProvider>
 	);
