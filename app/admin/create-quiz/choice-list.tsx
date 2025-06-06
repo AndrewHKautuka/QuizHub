fd766d01f6934f8addd69990d8e7bd67384c1958
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-06-06 13:10:05 +0200

    Fix choice field list
    - Add and apply RadioGroup Controller
    - Refactor styling

--- app/admin/create-quiz/choice-list.tsx
+++ app/admin/create-quiz/choice-list.tsx
@@ -22,53 +22,56 @@ export default function ChoicesList(props: ChoicesListProps) {
 	});
 	
 	return (
-		<RadioGroup defaultValue={getValues().questions[props.parentIndex].correctChoice}>
-			<Stack gap={2}>
-				{fields.map((field, index) => (
-					<Stack key={field.id} gap={2}>
-						<Stack direction="row" gap={2} sx={{ alignItems: "center", justifyContent: "space-between" }}>
-							<Radio value={index} />
-							
-							<Typography variant="body1">
-								{`Choice #${index + 1}`}
-							</Typography>
-							
-							<Controller
-								name={`questions.${props.parentIndex}.choices.${index}.text` as const}
-								control={control}
-								rules={{ required: fieldRequired, pattern: quizFieldPattern, maxLength: maxChoiceLength }}
-								render={({ field }) => (
-									<TextField
-										{...field}
-										label="Choice Text"
-										disabled={getValues().questions[props.parentIndex].type === "true/false"}
-										error={errors.questions?.[props.parentIndex]?.choices?.[index]?.text !== undefined}
-										helperText={errors.questions?.[props.parentIndex]?.choices?.[index]?.text?.message}
-									/>
-								)}
-							/>
-							
-							{
-								fields.length <= 2 ? <Box /> : (
+		<Controller
+			name={`questions.${props.parentIndex}.correctChoice`}
+			control={control}
+			render={(field) => (
+			<RadioGroup {...field} defaultValue={getValues().questions[props.parentIndex].correctChoice}>
+				<Stack gap={2}>
+					{fields.map((field, index) => (
+						<Stack key={field.id} gap={2}>
+							<Stack direction="row" gap={2} sx={{ alignItems: "center", justifyContent: "space-between" }}>
+								<Stack direction="row" gap={2} sx={{ alignItems: "center", justifyContent: "space-between" }}>
+									<Radio value={index} />
+									
+									<Typography variant="body1">
+										{`${index + 1}.`}
+									</Typography>
+								</Stack>
+								
+								<Controller
+									name={`questions.${props.parentIndex}.choices.${index}.text` as const}
+									control={control}
+									rules={{ required: fieldRequired, pattern: quizFieldPattern, maxLength: maxChoiceLength }}
+									render={({ field }) => (
+										<TextField
+											{...field}
+											label="Choice Text"
+											disabled={getValues().questions[props.parentIndex].type === "true/false"}
+											error={errors.questions?.[props.parentIndex]?.choices?.[index]?.text !== undefined}
+											helperText={errors.questions?.[props.parentIndex]?.choices?.[index]?.text?.message}
+										/>
+									)}
+								/>
+								
+								{fields.length <= 2 ? <Box /> : (
 									<IconButton onClick={() => remove(index)}>
 										<DeleteIcon />
 									</IconButton>
-								) 
-							}
+								)}
+							</Stack>
 						</Stack>
-					</Stack>
-				))}
-				
-				{
-					getValues().questions[props.parentIndex].type === "true/false" ? <></> : (
+					))}
+					
+					{getValues().questions[props.parentIndex].type === "true/false" ? <></> : (
 						<Box>
 							<Button color="secondary" variant="outlined" startIcon={<AddIcon />} onClick={() => append(defaultChoice)}>
 								Add New Choice
 							</Button>
 						</Box>
-					)
-				}
-			</Stack>
-		</RadioGroup>
+					)}
+				</Stack>
+			</RadioGroup>
+		)}/>
 	);
 }
