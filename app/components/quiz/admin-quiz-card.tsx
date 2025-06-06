36a72f3f7a20d3818c41fe1d1d9bb9c7ca36b040
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-06-06 13:20:08 +0200

    Update Admin Quiz Card details

--- app/components/quiz/admin-quiz-card.tsx
+++ app/components/quiz/admin-quiz-card.tsx
@@ -1,5 +1,6 @@
-import { Card, CardActionArea, CardContent, CardHeader, Grid, Typography } from "@mui/material";
+import { Button, Card, CardActionArea, CardActions, CardContent, CardHeader, Grid, Stack, Typography } from "@mui/material";
 import type { Tables } from "database.types";
+import { useNavigate } from "react-router";
 
 interface QuizCardProps {
 	quiz: Tables<"Quiz">;
@@ -7,19 +8,31 @@ interface QuizCardProps {
 };
 
 export default function AdminQuizCard(props: QuizCardProps) {
-	const onClick = () => {
-		
+	const navigate = useNavigate();
+	
+	const onClickView = () => {
+		navigate(`/admin/quiz/${props.quiz.quiz_id}`);
 	};
 	
 	return (
 		<Grid>
 			<Card raised>
+				<Stack className="h-full" sx={{ justifyContent: "space-between", padding: 1 }}>
+					<CardHeader title={props.quiz.title} subheader={props.questionsCount === 1 ? "1 question" : `${props.questionsCount} questions`} />
+					
 					<CardContent sx={{ textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
 						<Typography variant="body2">
 							{props.quiz.description}
 						</Typography>
 					</CardContent>
-				{/* </CardActionArea> */}
+					
+					<CardActions>
+						<Stack className="w-full" direction="row" gap={2} sx={{ justifyContent: "space-between" }}>
+							<Button variant="outlined" color="primary" onClick={onClickView}>View</Button>
+							<Button variant="text" color="secondary">Edit</Button>
+						</Stack>
+					</CardActions>
+				</Stack>
 			</Card>
 		</Grid>
 	);
